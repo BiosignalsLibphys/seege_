673ae8a9161ad386b44b9673e0d46c3e02e11f4b
Inês Silveira <66627966+inesapsilveira@users.noreply.github.com>
2025-10-07 12:29:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,151 @@
 
 import pickle
-from src.preprocessing import *
-from src.amplitude_similarity import *
-from src.frequency_similarity import *
-from src.scalogram_similarity import *
-from src.fractal_similarity import *
-from src.diversity import *
-from src.privacy import *
+from itertools import product
 
-def load_pickle(file_path):
-    """
-    Load a pickle file and return its contents.
-    """
-    with open(file_path, "rb") as f:
-        return pickle.load(f)
+from preprocessing import *
+from amplitude_similarity import *
+from frequency_similarity import *
+from scalogram_similarity import *
+from fractal_similarity import *
+from time_similarity import *
+from diversity import *
+from privacy import *
+import evaluation_score
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # Load real and synthetic data
-    real_data = load_pickle("data/real_dataset_10.pkl")
-    synthetic_data = load_pickle("data/synthetic_dataset_10.pkl")
+    # Load real and synthetic data, 512 Hz, normalized between -1 and 1
+    real_data = load_pickle("/Users/is/PycharmProjects/seege_/data/real_dataset_10.pkl")
+    synthetic_data = load_pickle("/Users/is/PycharmProjects/seege_/data/synthetic_dataset_10.pkl")
 
-    # Preprocess real and synthetic data
-    real_data = preprocess_data(real_data, fs=512, target_duration=60)
-    synthetic_data = preprocess_data(synthetic_data, fs=512, target_duration=60)
+    print(np.shape(real_data))
+    print(np.shape(synthetic_data))
 
     ##### FIDELITY ANALYSIS #####
 
     # Compute amplitude similarity
     # Initialize class
-    asim = AmplitudeSimilarity(fs=2048)
+    #asim = AmplitudeSimilarity(fs=512)
     # Compute amplitude similarity metrics
-    metrics = asim.compute_metrics(real_data, synthetic_data)
-    asim.plot_metrics(metrics)
+    #metrics_dataset = asim.compute_metrics(real_data, synthetic_data)
+    #metrics_sample = asim.compute_metrics(real_data[0], synthetic_data[0])
+
+    # Plot amplitude similarity metrics
+    #dataset_plot = asim.plot_metrics(metrics_dataset)
+    #sample_plot = asim.plot_metrics(metrics_sample)
+
+    # Compute amplitude similarity score
+    #dataset_score = evaluation_score.compute_amplitude_similarity_score(real_data, synthetic_data, fs=512)
+    #sample_score = evaluation_score.compute_amplitude_similarity_score(real_data[0], synthetic_data[0], fs=512)
+
+    # Compute time similarity
+    # Initialize class
+    #sim = TimeSimilarity()
+    # Compute amplitude similarity metrics
+    #results_dataset = sim.compute_time_metrics(real_data, synthetic_data, verbose=True)
+
+    # Plot Hjorth parameter distributions
+    #hjorth_hist_dataset = sim.plot_hjorth_histograms(real_data, synthetic_data)
+
+    # Plot 3D Hjorth parameter scatter
+    #hjorth_3d_dataset = sim.plot_hjorth_3d(real_data, synthetic_data)
+
+    # Compute time similarity score
+    #evaluation_score.compute_time_similarity_score(real_data, synthetic_data)
 
     # Compute frequency similarity
     # Initialize class
-    frequency_analysis = FrequencySimilarity(fs=2048)
+    #frequency_analysis = FrequencySimilarity(fs=512)
+    # Compute frequency similarity metrics
     # At dataset level
-    frequency_analysis.compare_relative_power(real_data, synthetic_data)
-    frequency_analysis.plot_psd(real_data, synthetic_data, scale="linear")
-    frequency_analysis.spectral_coherence(real_data[0], synthetic_data[0])
+    #frequency_analysis.compare_relative_power(real_data, synthetic_data)
+    #frequency_analysis.spectral_coherence(real_data, synthetic_data)
     # At sample level
-    frequency_analysis.compare_relative_power(real_data[0], synthetic_data[0])
-    frequency_analysis.plot_psd(real_data[5], synthetic_data[5], scale="log")
+    #frequency_analysis.compare_relative_power(real_data[0], synthetic_data[0])
+    #frequency_analysis.spectral_coherence(real_data[0], synthetic_data[0])
+
+    #Plot average PSD
+    # At dataset level
+    #frequency_analysis.plot_psd(real_data, synthetic_data, scale="linear")
+    # At sample level
+    #frequency_analysis.plot_psd(real_data[5], synthetic_data[5], scale="log")
+
+    # Compute frequency similarity score
+    # At dataset level
+    #evaluation_score.compute_frequency_similarity_score(real_data, synthetic_data, fs=512)
+    # At sample level
+    #evaluation_score.compute_frequency_similarity_score(real_data[0], synthetic_data[0], fs=512)
 
     # Compute scalogram similarity
     # Initialize class
-    scalogram_analysis = ScalogramSimilarity(fs=512)
+    #scalogram_analysis = ScalogramSimilarity(fs=512)
 
-    # Plot scalogram - sample level only
-    scalogram_analysis.plot_scalogram(real_data, synthetic_data, signal_index_real=0, signal_index_synth=0)
+    # Plot scalograms
+    # At sample level
+    #scalogram_analysis.plot_scalograms(real_data, synthetic_data, signal_index_real=0, signal_index_synth=1)
+    # At dataset level
+    #scalogram_analysis.plot_mean_scalograms(real_data,synthetic_data, freq_scale="log")
+
 
     # Compute scalogram similarity metrics
     # At dataset level
-    scalogram_analysis.compute_scalogram_similarity_metrics(real_data,synthetic_data)
+    #scalogram_analysis.compute_scalogram_similarity_metrics(real_data,synthetic_data)
     # At sample level
-    scalogram_analysis.compute_scalogram_similarity_metrics(real_data[0], synthetic_data[5])
+    #scalogram_analysis.compute_scalogram_similarity_metrics(real_data[0], synthetic_data[0])
+
+    # Compute scalogram similarity score
+    #evaluation_score.compute_scalogram_similarity_score(real_data[1], synthetic_data[3], fs=512)
+    #evaluation_score.compute_scalogram_similarity_score(real_data, synthetic_data, fs=512, mode="all_vs_all")
 
     # Compute fractality similarity
-    # Initialize class for DCCA method
-    fs_mfdcca = FractalSimilarity(real_data, synthetic_data, method='MFDCCA')
-    # Compute fractality similarity metrics - dataset level only
-    fs_mfdcca.analyze()
-
-    # Initialize class for DCCA method
-    fs_dcca = FractalSimilarity(real_data, synthetic_data, method='DCCA')
-    # Compute fractality similarity metrics - dataset level only
-    fs_dcca.analyze()
 
     # Initialize class for MFDFA method
-    fs_mfdfa = FractalSimilarity(real_data, synthetic_data, method='MFDFA')
-    # Compute fractality similarity metrics - dataset level only
-    fs_mfdfa.analyze()
+    #fs = FractalSimilarity(real_data, synthetic_data, method='MFDFA')
+    # Compute MFDFA metrics
+    #fs.compute_fractal_metrics()
+
+    # Initialize class for DCCA method
+    #fs = FractalSimilarity(real_data, synthetic_data, method='DCCA')
+    # Compute DCCA metrics
+    #fs.compute_fractal_metrics()
+    # Plot DCCA metrics
+    #fs.plot_metrics()
+
+    # Initialize class for MFDCCA method
+    #fs = FractalSimilarity(real_data, synthetic_data, method='MFDCCA')
+    # Compute MFDCCA metrics
+    #fs.compute_fractal_metrics()
+    # Plot MFDCCA metrics
+    #fs.plot_metrics()
+
+    # Compute fractalility similarity score
+    #evaluation_score.compute_fractality_score(real_data, synthetic_data)
+
+    # Compute fidelity score
+    #evaluation_score.compute_fidelity_score(real_data, synthetic_data, fs=2048)
 
     ##### DIVERSITY ANALYSIS #####
     # Initialize class
-    diversity_eval = Diversity(n_components=2)
+    #div = Diversity()
 
-    # Plot PCA, t-SNE, and UMAP results
-    diversity_eval.plot_pca()
-    diversity_eval.plot_tsne()
-    diversity_eval.plot_umap()
+    # Compute diversity metrics
+    #div_results = div.compute_metrics(real_data, synthetic_data)
+
+    # Plot PCA and UMAP results
+    #div.plot_embeddings("PCA", div_results)
+    #div.plot_embeddings("UMAP", div_results)
+
+    # Compute diversity score
+    #evaluation_score.compute_diversity_score(real_data, synthetic_data)
 
     ##### PRIVACY ANALYSIS #####
     ## Initialize class
-    privacy_analysis = Privacy()
+    #privacy_analysis = Privacy()
 
-    # Compute privacy metrics
-    privacy_metrics = privacy_analysis.compute_privacy_metrics(real_data,synthetic_data)
+    # Compute distance and MIR metrics
+    #distance_metrics = privacy_analysis.compute_distance_metrics(real_data, synthetic_data)
+    #mir = privacy_analysis.compute_mir(real_data, synthetic_data)
+
+    # Compute privacy score
+    #evaluation_score.compute_privacy_score(real_data, synthetic_data)
+
